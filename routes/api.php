9159f97817a323d4d2581ed68b2f58167602c393
wunai <1179485598@qq.com>
2019-02-18 16:16:51 +0800

    1

--- routes/api.php
+++ routes/api.php
@@ -22,20 +22,19 @@ Route::any('/test', 'HJGL\API\WeChatController@test');
 Route::any('/getAccessToken', 'HJGL\API\WeChatController@getAccessToken');
 
 Route::group(['middleware'=>['web','wechat.oauth']],function(){
+    Route::any('/sendAlertMsg', 'HJGL\API\WeChatController@sendAlertMsg');
+    Route::any('/wechat', 'HJGL\API\WeChatController@serve');
+    Route::get('/getAccessToken', 'HJGL\API\WeChatController@getAccessToken');//获取access_token
+    Route::get('/getMenu', 'HJGL\API\WeChatController@getMenu');//获取菜单
+    Route::get('/createMenu', 'HJGL\API\WeChatController@createMenu');//创建菜单
+    Route::get('/delMenu', 'HJGL\API\WeChatController@delMenu');//删除菜单
+    Route::get('/webScope', 'HJGL\API\WeChatController@webScope');//网页授权
+    Route::get('/getInfo', 'HJGL\API\WeChatController@getInfo');//获取信息
 
+    
+    Route::get('/hjjc', 'HJGL\API\WeChatController@hjjc');//环境检测
 });
-Route::any('/sendAlertMsg', 'HJGL\API\WeChatController@sendAlertMsg');
-Route::any('/wechat', 'HJGL\API\WeChatController@serve');
-Route::get('/getAccessToken', 'HJGL\API\WeChatController@getAccessToken');//获取access_token
-Route::get('/getMenu', 'HJGL\API\WeChatController@getMenu');//获取菜单
-Route::get('/createMenu', 'HJGL\API\WeChatController@createMenu');//创建菜单
-Route::get('/delMenu', 'HJGL\API\WeChatController@delMenu');//删除菜单
-Route::get('/webScope', 'HJGL\API\WeChatController@webScope');//网页授权
-Route::get('/getInfo', 'HJGL\API\WeChatController@getInfo');//获取信息
 
-
-
-Route::get('/hjjc', 'HJGL\API\WeChatController@hjjc');//环境检测
 
 
 
