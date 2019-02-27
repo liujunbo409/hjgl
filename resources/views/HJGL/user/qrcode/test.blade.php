48c04103c60356fb2561f7c8f5e60b43ecab9de9
wunai <1179485598@qq.com>
2019-02-27 16:38:14 +0800

    1

--- resources/views/HJGL/user/qrcode/test.blade.php
+++ resources/views/HJGL/user/qrcode/test.blade.php
@@ -11,27 +11,27 @@
 @endsection
 
 @section('script')
-    <script src="http://res.wx.qq.com/open/js/jweixin-1.4.0.js" type="text/javascript" charset="utf-8"></script>
+    <script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript" charset="utf-8"></script>
     <script type="text/javascript" charset="utf-8">
-        {{--wx.config(<?php echo $app->jssdk->buildConfig(array('onMenuShareQQ', 'onMenuShareWeibo'), true) ?>);--}}
-        wx.config({
-            debug: true,
-            appId: "{{$data['appId']}}",
-            timestamp: "{{$data['timestamp']}}",
-            nonceStr: "{{$data['nonceStr']}}",
-            signature: "{{$data['signature']}}",
-            jsApiList: "{{$data['jsApiList']}}"
-        });
+        wx.config(<?php echo $app->jssdk->buildConfig(array('onMenuShareQQ', 'onMenuShareWeibo'), true) ?>);
+        {{--wx.config({--}}
+            {{--debug: true,--}}
+            {{--appId: "{{$data['appId']}}",--}}
+            {{--timestamp: "{{$data['timestamp']}}",--}}
+            {{--nonceStr: "{{$data['nonceStr']}}",--}}
+            {{--signature: "{{$data['signature']}}",--}}
+            {{--jsApiList: "{{$data['jsApiList']}}"--}}
+        {{--});--}}
         console.log(wx.config);
-        wx.ready(function () {
-            wx.checkJsApi({
-                jsApiList: ['checkJsApi','openLocation'],
-                success: function (res) {}
-            });
-        });
-        wx.error(function(res){
-            console.log(res);
-        });
+        {{--wx.ready(function () {--}}
+            {{--wx.checkJsApi({--}}
+                {{--jsApiList: ['checkJsApi','openLocation'],--}}
+                {{--success: function (res) {}--}}
+            {{--});--}}
+        {{--});--}}
+        {{--wx.error(function(res){--}}
+            {{--console.log(res);--}}
+        {{--});--}}
     </script>
     <script type="text/javascript">
 
