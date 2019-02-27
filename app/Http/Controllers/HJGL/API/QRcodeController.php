2505f5d1e52af271546763d3d759d2885767dda4
wunai <1179485598@qq.com>
2019-02-27 15:28:38 +0800

    1

--- app/Http/Controllers/HJGL/API/QRcodeController.php
+++ app/Http/Controllers/HJGL/API/QRcodeController.php
@@ -3,12 +3,28 @@ namespace App\Http\Controllers\HJGL\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Config;
+use EasyWeChat\Factory;
 
 
 class QRcodeController extends Controller{
 
-    public function test(){
-        return view('HJGL.user.qrcode.test');
+    public function test(Request $request){
+        $jsapi_ticket = cache('jsapi_ticket', null);
+        if(empty($jsapi_ticket)){
+            $config = Config::get("wechat.official_account.default");
+            $app = Factory::officialAccount($config);
+            $accessToken = $app->access_token;
+            $token = $accessToken->getToken();
+            $url='https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='.$token['access_token'].'&type=jsapi';
+            $json = file_get_contents($url);
+            $re = json_decode($json);
+            $jsapi_ticket = $re->ticket;
+            cache(['jsapi_ticket'=>$re->ticket],$re->expires_in - 200);
+        }
+        dd($jsapi_ticket);
+        return view('HJGL.user.qrcode.test',['jsapi_ticket'=>$jsapi_ticket]);
     }
 
     public function index($tool_id){
--- resources/views/HJGL/user/qrcode/test.blade.php
+++ resources/views/HJGL/user/qrcode/test.blade.php
@@ -11,6 +11,18 @@
 @endsection
 
 @section('script')
+    <script src="http://res.wx.qq.com/open/js/jweixin-1.4.0.js" type="text/javascript" charset="utf-8"></script>
+    <script type="text/javascript" charset="utf-8">
+        wx.config({
+            debug: true,
+            appId: 'wx3cf0f39249eb0e60',
+            timestamp: 1430009304,
+            nonceStr: 'qey94m021ik',
+            signature: '4F76593A4245644FAE4E1BC940F6422A0C3EC03E',
+            jsApiList: ['updateAppMessageShareData', 'updateTimelineShareData']
+        });
+        console.log(wx.config);
+    </script>
     <script type="text/javascript">
 
     </script>
