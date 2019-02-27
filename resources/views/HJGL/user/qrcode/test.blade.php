85e760e836e7618c3106d3a870c9669531891bec
wunai <1179485598@qq.com>
2019-02-27 16:46:57 +0800

    1

--- resources/views/HJGL/user/qrcode/test.blade.php
+++ resources/views/HJGL/user/qrcode/test.blade.php
@@ -13,7 +13,9 @@
 @section('script')
     <script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript" charset="utf-8"></script>
     <script type="text/javascript" charset="utf-8">
-        wx.config(<?php echo $app->jssdk->buildConfig(array('onMenuShareQQ', 'onMenuShareWeibo'), true) ?>);
+        wx.config(<?php use Illuminate\Support\Facades\Config;
+            use EasyWeChat\Factory; $config = Config::get("wechat.official_account.default");
+            $app = Factory::officialAccount($config); echo $app->jssdk->buildConfig(array('onMenuShareQQ', 'onMenuShareWeibo'), true) ?>);
         {{--wx.config({--}}
             {{--debug: true,--}}
             {{--appId: "{{$data['appId']}}",--}}
